dbdcc591edcaedf7b25a53dc9564bd93ee18ad9a
RadonZ <supachai27102000@gmail.com>
2024-02-14 21:59:48 +0700

    make new line

--- Basic/01-variable-and-project-band-random/project/main.py
+++ Basic/01-variable-and-project-band-random/project/main.py
@@ -1,9 +1,9 @@
 #1. Create a greeting for your program.
 print("Hello may I interrupt your time")
 #2. Ask the user for the city that they grew up in.
-city = input("What is your city you grow up ")
+city = input("What is your city you grow up?\n")
 #3. Ask the user for the name of a pet.
-pet = input("What is your pet name ")
+pet = input("What is your pet name?\n")
 #4. Combine the name of their city and pet and show them their band name.
 print(city+" "+ pet)
 #5. Make sure the input cursor shows on a new line:

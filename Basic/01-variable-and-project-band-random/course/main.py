f867a25950932711550c2be33cd03b10bda71c10
RadonZ <supachai27102000@gmail.com>
2024-02-14 21:55:27 +0700

    add resource and project

---
+++ Basic/01-variable-and-project-band-random/course/main.py
@@ -0,0 +1,33 @@
+# print
+print("Hello" + " " +"Angela");
+
+# input() will get user input in console
+# Then print() will print the word "Hello" and the user input
+print("Hello " + input("What is your name?"))
+
+#variable
+# name = input("What is your name? ")
+# print(name)
+
+# if has 2 variable
+name= "Jack"
+print(name);
+
+name= "Angela"
+print(name);
+
+# len() code
+
+name = input("What is your name?")
+length = len(name)
+print(length)
+# print number word
+
+# variable
+#use word make the same color
+#make easy to see
+name1 = "Jack"
+print(name1)
+
+name2 = "Lola"
+print(name2)
---
+++ Basic/01-variable-and-project-band-random/project/main.py
@@ -0,0 +1,9 @@
+#1. Create a greeting for your program.
+print("Hello may I interrupt your time")
+#2. Ask the user for the city that they grew up in.
+city = input("What is your city you grow up ")
+#3. Ask the user for the name of a pet.
+pet = input("What is your pet name ")
+#4. Combine the name of their city and pet and show them their band name.
+print(city+" "+ pet)
+#5. Make sure the input cursor shows on a new line:
